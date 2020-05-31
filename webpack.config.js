eab7b1bc73bbf0f6eb760483beef4e50269f11de
Kenichi Ishii <36427212+kenichi-77@users.noreply.github.com>
2020-05-31 15:09:52 +0900

    Buikd css with sass

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,16 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css/,
+        test: /\.(css|sass|scss)/,
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
           },
         ],
       },
@@ -62,6 +65,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./src/templates/access.pug",
       filename: "access.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/templates/members/taro.pug",
+      filename: "taro.html",
     }),
     new CleanWebpackPlugin(),
   ],
